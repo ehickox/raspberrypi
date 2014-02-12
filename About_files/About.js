c32f582092c218c8f5c17b0542572b610ad0efa9
ehickox2012 <ehickox2012@aol.com>
2014-02-12 12:19:43 -0600

    updated site to include a file

--- About_files/About.js
+++ About_files/About.js
@@ -1,9 +1,9 @@
-// Created by iWeb 3.0.4 local-build-20140211
+// Created by iWeb 3.0.4 local-build-20140212
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('About_files/AboutMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
--- Home_files/Home.js
+++ Home_files/Home.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20140211
+// Created by iWeb 3.0.4 local-build-20140212
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
